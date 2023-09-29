3ae3299c7beed4c5a5ef93561cb1b5e1a5792306
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-09-29 20:09:21 +0900

    devise root追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
 
-  # Defines the root path route ("/")
-  # root to: "home#index"
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+    # root "users/sessions#new"
+    root "users/registrations#new"
+  end
 end
