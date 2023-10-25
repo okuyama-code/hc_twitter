3446b570b6f6bd71cda39f6f9fbec5efce37ae47
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-10-25 12:05:53 +0900

    github認証一応できた

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,6 +18,8 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def after_sign_out_path_for(_)
+
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,18 @@ class User < ApplicationRecord
   has_many :authorizations, dependent: :destroy
 
   def self.from_omniauth(auth)
-    authorization = Authorization.find_or_initialize_by(provider: auth.provider, uid: auth.uid)
-    authorization.assign_attributes(name: auth.info.name, email: auth.info.email)
+    authorization = User.find_or_initialize_by(provider: auth.provider, uid: auth.uid)
+    authorization.assign_attributes(full_name: auth.info.name, email: auth.info.email)
 
     where(email: auth.info.email).first_or_initialize.tap do |user|
-      user.user_name = auth.info.name
-      user.remote_profile_url = auth.info.image if auth.info.image.present?
+      user.full_name = auth.info.name
+      user.avatar_url = auth.info.image if auth.info.image.present?
+      user.password = Devise.friendly_token[0, 20]
+      user.date_of_birth = "1998-01-21"
+      user.telephone = "08000001111"
       user.save!
-      user.authorizations << authorization unless user.authorizations.exists?(provider: auth.provider, uid: auth.uid)
+
     end
   end
 
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.email = auth.info.email
-  #     user.password = Devise.friendly_token[0, 20]
-  #   end
-  # end
 end
