e84d0c2b59261630276dfd8da82cf2f98035e859
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-11-21 04:54:36 +0900

    DMコントローラー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,27 @@ class UsersController < ApplicationController
     @like_posts = @user.likes.order(created_at: :desc).page(params[:page]).per(3)
     @repost_posts = @user.reposts.order(created_at: :desc).page(params[:page]).per(3)
     @commnet_posts = @user.comments.order(created_at: :desc).page(params[:page]).per(3)
+
+    # DM機能についての記述
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+    end
+
+    if @isRoom
+    else
+      @room = Room.new
+      @entry = Entry.new
+    end
   end
 
   def followings
