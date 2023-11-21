f98870adb054eada50b42bb2e47997dd4afdae04
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-11-21 17:51:45 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,24 +10,25 @@ class UsersController < ApplicationController
     @commnet_posts = @user.comments.order(created_at: :desc).page(params[:page]).per(3)
 
     # DM機能についての記述
-    @currentUserEntry = Entry.where(user_id: current_user.id)
-    @userEntry = Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
-    else
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
+    @current_entry = Entry.where(user_id: current_user.id)
+    @another_entry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_entry.each do |current|
+        @another_entry.each do |another|
+          #もし現在のユーザーと表示しているユーザーが同じルームにエントリーしていたら
+          if current.room_id == another.room_id
+            # @is_room は同じルームにエントリーがあるかどうかを示すフラグで、初期値は false
+            @is_room = true
+            @room_id = current.room_id
           end
         end
       end
-    end
-
-    if @isRoom
-    else
-      @room = Room.new
-      @entry = Entry.new
+      # @is_room が false の場合に中の処理を実行(同じルームにエントリーがない場合)
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+        #同じルームにエントリーがない場合に、新しいルームとエントリーが作成され、それを後続の処理で利用できるようになる。
+      end
     end
   end
 
