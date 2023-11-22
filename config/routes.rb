951288bd7205d76eed22f0dc9e8f47afc9403421
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-11-22 09:51:25 +0900

    DM機能のlayoutセット

--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,8 @@ Rails.application.routes.draw do
   end
 
   resources :messages, only: [:create]
-  resources :rooms, only: [:create, :show]
+  resources :rooms, only: [:create, :index, :show]
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root 'posts#index'
-  get '/message' => 'home#message'
-  get '/message_box' => 'home#message_box'
 end
