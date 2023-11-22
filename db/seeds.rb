7250fd4172f6c3b6b02853c05ad9e054b841b2d8
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-11-22 18:59:38 +0900

    messageの左右完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ yamada.icon.attach(io: File.open(Rails.root.join('app/assets/images/EC2.jpg')), 
 yamada.header.attach(io: File.open(Rails.root.join('app/assets/images/suisu1.jpg')), filename: 'suisu1.jpg')
 yamada.save!
 
-# =================== yamada =================
+# =================== suzuki =================
 suzuki = User.create!(name: "suzuki", username: "suzuki0101", email: "suzuki@gmail.com", password: "484848", date_of_birth: '1990-01-01', telephone: '09011112222', self_introduction: "はじめまして！suzukiです！", location: "宮城", website: "suzuki-portfolio-site.vercel.app")
 
 suzuki.icon.attach(io: File.open(Rails.root.join('app/assets/images/EC3.jpg')), filename: 'EC3.jpg')
