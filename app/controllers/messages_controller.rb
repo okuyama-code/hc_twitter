e47a1bc8668a15bf2fa3bb94a8b62a71910b1164
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-11-22 12:22:59 +0900

    message作成完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,24 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    pp "デバック！！！！！！！！！！！！！"
+    pp params
+    @message = Message.new(message_params)
+    @message.room_id = params[:room_id]
+    @message.user_id = current_user.id
+    if @message.save!
+      flash[:notice] = "メッセージを投稿しました。"
+      redirect_to request.referer
+    else
+      flash[:notice] = "メッセージの投稿に失敗しました"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
 end
