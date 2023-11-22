f875e3d350572d2bc33d22f90f06bd180a12ef3e
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-11-23 05:38:01 +0900

    通知作成メソッド完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,17 @@ class User < ApplicationRecord
     reverse_of_relationships.find_by(following_id: user.id).present?
   end
 
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
+  end
+
 
   def self.from_omniauth(auth)
     pp "デバック！！！！！！！！！！！！！"
