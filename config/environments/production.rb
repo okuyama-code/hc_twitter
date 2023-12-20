d3afe67e7a358200611cf93ce719808849a62b2a
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-20 10:27:13 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  onfig.action_mailer.default_url_options = { protocol: 'https',
-                                              host: 'floating-garden-19803-96c1af154bf2.herokuapp.com' }
+  # config.action_mailer.default_url_options = { protocol: 'https',
+  #                                             host: 'floating-garden-19803-96c1af154bf2.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
