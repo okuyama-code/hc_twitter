d4e82044af150eab510f988c0058489e49d96e0a
okuyama-code <ooyy0121@gmail.com>
2024-03-21 19:25:53 +0900

    cssなど修正

--- db/schema.rb
+++ db/schema.rb
@@ -90,10 +90,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_183728) do
   end
 
   create_table "notifications", force: :cascade do |t|
-    t.integer "visitor_id", null: false
-    t.integer "visited_id", null: false
-    t.integer "post_id"
-    t.integer "comment_id"
+    t.bigint "visitor_id"
+    t.bigint "visited_id"
+    t.bigint "post_id"
+    t.bigint "comment_id"
     t.string "action", default: "", null: false
     t.boolean "checked", default: false, null: false
     t.datetime "created_at", null: false
@@ -113,10 +113,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_183728) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "following_id"
-    t.integer "follower_id"
+    t.bigint "follower_id"
+    t.bigint "following_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
   create_table "reposts", force: :cascade do |t|
@@ -182,7 +184,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_183728) do
   add_foreign_key "likes", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "notifications", "users", column: "visited_id"
+  add_foreign_key "notifications", "users", column: "visitor_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "relationships", "users", column: "follower_id"
+  add_foreign_key "relationships", "users", column: "following_id"
   add_foreign_key "reposts", "posts"
   add_foreign_key "reposts", "users"
   add_foreign_key "rooms", "users"
